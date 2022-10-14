6eeb99412144855da6a85f59d9ab88d1a316545e
Isaac Rahel <isaac_rahel@live.com>
2022-10-14 14:58:16 -0300

    feat: adding hello

--- Program.cs
+++ Program.cs
@@ -1,14 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
-var port = builder.Configuration["PORT"];
-// Add services to the container.
 
+var port = builder.Configuration["PORT"];
+
+
+//set listening urls
+builder.WebHost.UseUrls($"http://*:{port};http://localhost:3000");
+
+
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
+app.MapGet("/",()=>"Hello world 🥂");
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
